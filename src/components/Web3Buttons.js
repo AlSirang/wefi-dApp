ae84d684993f11c99c4ddf12dc96c22b2ceba91a
AlSirang <uddinbashir1999@gmail.com>
2022-10-26 14:27:35 +0500

    fix: add helper components

---
+++ src/components/Web3Buttons.js
@@ -0,0 +1,44 @@
+import { WalletConnect } from "./buttons";
+import { switchNetwork } from "../context/utils";
+import { Web3UserContext } from "../context";
+
+const Web3Buttons = ({
+  hideWalletConnectButton = false,
+  walletConnectClasses = "",
+}) => {
+  const {
+    contextState: {
+      isWalletConnected,
+      isCorrectChain,
+
+      provider,
+    },
+  } = Web3UserContext();
+  return (
+    <>
+      {!hideWalletConnectButton && (
+        <div className={`${walletConnectClasses} justify-content-center`}>
+          <WalletConnect
+            className="button-base primary-button connect-wallet"
+            style={{ maxWidth: 250 }}
+          />
+        </div>
+      )}
+
+      {isWalletConnected && !isCorrectChain && (
+        <div className="mt-3">
+          <p>Please switch network to correct chain.</p>
+          <button
+            onClick={switchNetwork.bind(this, provider)}
+            className="button-base secondary-button"
+            style={{ maxWidth: 200 }}
+          >
+            Switch Network
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Web3Buttons;

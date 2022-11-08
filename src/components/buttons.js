a8d7a6556ecc7a8d800685752d74b006de091202
AlSirang <uddinbashir1999@gmail.com>
2022-11-08 15:18:29 +0500

    fix: add button to manage wallet assests

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { ButtonUserContext, Web3UserContext } from "../context";
 import { shortenAddress } from "../utils/constants";
+import metamaskIcon from "../assets/img/metamask.svg";
+import "../styles/metamaskButton.css";
 
 export const WalletConnect = (props) => {
   const {
@@ -29,6 +31,32 @@ export const BuyNowButton = () => {
   return (
     <button onClick={navigateToPresale} className="button-base primary-button">
       {name}
+    </button>
+  );
+};
+
+export const AssetManagmentButton = ({ name, options }) => {
+  const {
+    contextState: { provider },
+  } = Web3UserContext();
+  const handleClick = async () => {
+    try {
+      await provider.request({
+        method: "wallet_watchAsset",
+        params: {
+          type: "ERC20", // Initially only supports ERC20, but eventually more!
+          options,
+        },
+      });
+    } catch (e) {}
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="btn btn-xss btn-soft-light text-nowrap d-flex align-items-center mr-2"
+    >
+      <img class="mr-1" width="15" src={metamaskIcon} alt="Metamask" />
+      &nbsp;{name}
     </button>
   );
 };
